e4caf0955bd770f70df8932881c4711ab045836b
hosseinemir <hosseinemir79@gmail.com>
2023-09-11 04:10:55 +0330

    change buy page to get data serverside from its side

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,14 +1,19 @@
+import Profile from '@/models/Profile';
 import BuyResidentialsPage from '@/template/BuyResidentialsPage'
+import ConnectDB from '@/utils/ConnectDB';
 import React from 'react'
 
 export default async function BuyResidential({searchParams}) {
-  const res = await fetch("https://amlak-next-hosseinemir.vercel.app/api/profile", {
-    cache: "no-store",
-  });
-  const data = await res.json()
-  if(data.error) return <h3>مشکلی پیش امده است</h3>
+  await ConnectDB();
+  const profile = await Profile.find({published:true}).select("-UserId")
 
-let prodata = data.data
+  // const res = await fetch("https://amlak-next-hosseinemir.vercel.app/api/profile", {
+  //   cache: "no-store",
+  // });
+  // const data = await res.json()
+  if(!profile) return <h3>مشکلی پیش امده است</h3>
+
+let prodata = profile
 if(searchParams.category){
   prodata = prodata.filter(item => item.category === searchParams.category)
 }
